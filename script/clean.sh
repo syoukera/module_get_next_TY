bc24bea98dd8fe1f5d7a59c937456343ecb03b4f
syoukera <s.akira2986@gmail.com>
2020-04-02 11:47:05 +0900

    Update compile and clean script

---
+++ script/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm obj/*.o
+rm get_next_TY
+rm output/*out
+rm output/skout_datasheet
+rm output/save
--- script/compile.sh
+++ script/compile.sh
@@ -3,6 +3,6 @@
 gfortran -o obj/cklib.o -c src/cklib.f
 gfortran -o obj/dasac.o -c src/dasac.f
 gfortran -o obj/senkin.o -c src/senkin.f
-gfortran -o obj/main.o -c main.f
+gfortran -o obj/main.o -c main.F90
 
 gfortran -o get_next_TY obj/senkin.o obj/main.o obj/cklib.o obj/dasac.o
